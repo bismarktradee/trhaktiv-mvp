26f3c9a0bef4e3f879b76e2a841ade9acf217729
bismarktradee <39523939+bismarktradee@users.noreply.github.com>
2026-03-09 09:37:57 +0100

    Use environment variable for server port

--- app.py
+++ app.py
@@ -272,4 +272,5 @@ if __name__ == '__main__':
     print("Pro zastavení stiskněte Ctrl+C")
     print("="*70 + "\n")
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+app.run(debug=True, host='0.0.0.0', port=port)
